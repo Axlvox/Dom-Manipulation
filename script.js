41c410d68c909316665d6909122329facc20978d
Axlvox <leandro.jlds@gmail.com>
2023-04-11 22:56:28 -0300

    Adiciona Requisito 2

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function texto() {
     change.innerHTML = 'Trabalhar em uma BigTech';
 };
 texto();
+
+function alteraCor() {
+    let changeColor = document.getElementsByClassName('main-content')[0];
+    changeColor.style.background = 'rgb(76, 164, 109)';
+};
+alteraCor();
